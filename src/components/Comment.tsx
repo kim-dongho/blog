a422eff6173ac0034157bf60630ab676f904324c
김동호 <donghok@innogrid.com>
2024-09-23 16:52:05 +0900

    fix: utteranc pathname encode 3

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -2,17 +2,13 @@ import { useEffect, useRef } from "react";
 
 const Comment = () => {
   const commentsEl = useRef<HTMLDivElement>(null);
-  const slug = window.location.pathname
-    .replace(/[^\w\s-]/g, "")
-    .replace(/\s+/g, "-")
-    .toLowerCase();
 
   useEffect(() => {
     const scriptEl = document.createElement("script");
     scriptEl.async = true;
     scriptEl.src = "https://utteranc.es/client.js";
     scriptEl.setAttribute("repo", "kim-dongho/blog");
-    scriptEl.setAttribute("issue-term", slug);
+    scriptEl.setAttribute("issue-term", "title");
     scriptEl.setAttribute("theme", "github-light");
     scriptEl.setAttribute("crossorigin", "anonymous");
     commentsEl.current?.appendChild(scriptEl);
